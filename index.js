fa125f583bb390aec91b6666a25189a6e116b743
KVKYES <winstontroysings@gmail.com>
2016-02-15 23:22:38 -0500

    Worked on "Read More" "Read Less"

---
+++ index.js
@@ -0,0 +1,43 @@
+$ (document).ready(function() {
+
+
+/*$('#button').click(function() {
+	alert("We are not yet taking sign ups");
+});*/
+
+$('button').click(function(){
+	swal("Oops!", "We are not yet taking sign ups.", "error");
+});
+
+$('#button').click(function(){
+      $('#no_sign_ups').show();
+      $('#button').hide();
+  });
+
+$('#readmore1').click(function(){
+	event.preventDefault();
+	$('.content-post1_readmore1').show();
+	$('#readmore1').hide();
+	$('#readless1').show();
+});
+
+$('#readmore2').click(function(){
+	event.preventDefault();
+	$('.content-post2-readmore2').show();
+	$('#readmore2').hide();
+	$('#readless2').show();
+});
+
+$('#readless1').click(function(){
+	event.preventDefault();
+	$('.content-post1-readmore1').hide();
+	$('#readless1').hide();
+	$('#readmore1').show();
+});
+
+$('#readless2').click(function(){
+	event.preventDefault();
+	$('.content-post2-readmore2').hide();
+	$('#readless2').hide();
+	$('#readmore2').show();
+});
