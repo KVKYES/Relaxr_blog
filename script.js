95b91d5ccc7107a46128b70383c02698ce752850
KVKYES <winstontroysings@gmail.com>
2016-02-18 11:53:58 -0500

    Need help with the 'Sign Up' button

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 $ (document).ready(function() {
+	/*$('#button').click(function() {
+		alert("We are not yet taking sign ups");
+	});*/
 
+	
 
-/*$('#button').click(function() {
-	alert("We are not yet taking sign ups");
-});*/
+	// $('#button').click(function(){
+	//       $('#no_sign_ups').show();
+	//       $('#button').hide();
+	//   });
 
-$('button').click(function(){
-	swal("Oops!", "We are not yet taking sign ups.", "error");
-});
+	$('#readmore1').click(function(e){
+		e.preventDefault();
+		$('.content-post1-readmore1').show();
+		$('#readmore1').hide();
+		$('#readless1').show();
+	});
 
-$('#button').click(function(){
-      $('#no_sign_ups').show();
-      $('#button').hide();
-  });
+	$('#readmore2').click(function(){
+		event.preventDefault();
+		$('.content-post2-readm2').show();
+		$('#readmore2').hide();
+		$('#readless2').show();
+	});
 
-$('#readmore1').click(function(){
-	event.preventDefault();
-	$('.content-post1_readmore1').show();
-	$('#readmore1').hide();
-	$('#readless1').show();
-});
+	$('#readless1').click(function(e){
+		e.preventDefault();
+		$('.content-post1-readmore1').hide();
+		$('#readless1').hide();
+		$('#readmore1').show();
+	});
 
-$('#readmore2').click(function(){
-	event.preventDefault();
-	$('.content-post2-readmore2').show();
-	$('#readmore2').hide();
-	$('#readless2').show();
-});
+	$('#readless2').click(function(e){
+		event.preventDefault();
+		$('.content-post2-readmore2').hide();
+		$('#readless2').hide();
+		$('#readmore2').show();
+	});
 
-$('#readless1').click(function(){
-	event.preventDefault();
-	$('.content-post1-readmore1').hide();
-	$('#readless1').hide();
-	$('#readmore1').show();
-});
-
-$('#readless2').click(function(){
-	event.preventDefault();
-	$('.content-post2-readmore2').hide();
-	$('#readless2').hide();
-	$('#readmore2').show();
 });
